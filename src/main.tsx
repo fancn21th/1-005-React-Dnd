e72b1183b18c979bbf4cd2111b2a22d1bc4e4aee
fancn21th <fancn21th@gmail.com>
2022-10-26 17:04:26 +0800

    baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <>
+      <CssBaseline />
+      <App />
+    </>
   </React.StrictMode>
-)
+);
