5597620416d547eeb68267e48f1baa9bfc20fff0
fancn21th <fancn21th@gmail.com>
2022-10-26 11:11:38 +0800

    styled component integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import styled from "styled-components";
 import "./App.css";
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>dnd</h1>
-    </div>
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
   );
 }
 
